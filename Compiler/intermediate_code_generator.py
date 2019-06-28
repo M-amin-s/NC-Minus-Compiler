b7d48c6af12954d8c867aa8b3bd618b92ad798c1
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-06-28 14:34:20 +0430

    Semantic actions for variable and array declaration

--- Compiler/intermediate_code_generator.py
+++ Compiler/intermediate_code_generator.py
@@ -1,23 +1,49 @@
 ### INTERMEDIATE CODE GENERATOR FOR NC MINUS COMPILER ###
 
+
+ADD = 'ADD'
+SUB = 'SUB'
+AND = 'AND'
+ASSIGN = 'ASSIGN'
+EQ = 'EQ'
+JPF = 'JPF'
+JP = 'JP'
+LT = 'LT'
+MULT = 'MULT'
+NOT = 'NOT'
+PRINT = 'PRINT'
+INT = 'int'
+VOID = 'void'
+
+
 # current scope is a list of scopes from outer to inner at the time of execution
+# 0 is the program scope in which everything is defined at
 # scope id's start from 0 and every time we reach a new scope we add 1
 # to the top_of_stack and get push it on top of current_scope
 # every time we get out of a scope we pop from the current_scope
-current_scope = []
+scope = [0]
 
-# a list of pairs with the scope(list) in which the variables were defined at and the variable name
-# e.g. var_scope_stack=[('x', [0,1,2]), ('y', [0,1,3]), (['x', [0,1,2])]
+# the id of next scope
+scope_id = 1
+
+# a list of 4 pairs with the scope(list) in which the variables were defined at, the variable name,
+# the type of variable and the address of variable
+# e.g. var_scope_stack=[('x', [0,1,2], 'int', 540), ('y', [0,1,3], 'void', 544), (['x', [0,1,2], 'int', 548)]
 var_scope_stack = []
 
-# a list of 4 pairs with the scope(list) in which the functions were defined at, the function name,
-# the number of arguments and return type
-# e.g. func_scope_stack=[('add_func', [0,1], 2, 'int'), ('main', [0], 0, 'void')]
-# must include ('main', [0], 0, 'void') otherwise print error: "main function not found"
-func_scope_stack = []
+# a list of 5 paris with the scope(list) in which the arrays were defined at, the variable name,
+# the type of variable, the address of the start of array and the address of the end of array
+# e.g. arr_scope_stack=[('x_arr', [0,1,2,4], 'int', 540, 564)]
+arr_scope_stack = []
 
-# the array of 4 element pairs which needs to be filled with instructions
-# e.g. program_block=[('ADD', 500, 1000, 508), ('JPF', 512, 6)]
+# a list of 4 pairs with the scope(list) in which the functions were defined at, the function name,
+# the argument types and return type
+# e.g. func_scope_stack=[('add_func', [0,1], ['int', 'int'], 'int'), ('main', [0], [], 'void')]
+# must include ('main', [0], [], 'void') otherwise print error: "main function not found"
+func_scope_stack = [('output', [0], [INT], VOID)]
+
+# the array of (at most) 4 element pairs which needs to be filled with instructions
+# e.g. program_block=[('ADD', 500, 1000, 508), ('JPF', 512, 6, )]
 program_block = []
 
 # compilation time stack to be used by compiler
@@ -32,17 +58,6 @@ program_ptr = 0
 # current address of temporary memory (increment by 4)
 tmp_ptr = 1000
 
-ADD = 'ADD'
-SUB = 'SUB'
-AND = 'AND'
-ASSIGN = 'ASSIGN'
-EQ = 'EQ'
-JPF = 'JPF'
-JP = 'JP'
-LT = 'LT'
-MULT = 'MULT'
-NOT = 'NOT'
-PRINT = 'PRINT'
 
 
 
